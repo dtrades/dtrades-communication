26a5b410ba46cbde473f3fe2ea539636f4c89004
Denis Carriere <carriere.denis@gmail.com>
2018-09-22 13:25:16 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-var dotenv = require("dotenv");
-var path = require("path");
-var fs = require('fs');
+function serialize(buff){
 
-//entry point
-function eosCommunications(opts){
+	var str = "TO DECRYPT: msg.eostitan.com\n";
 
-	//validate parameters, fallback to configuration files, if all else fails, throw exception
-	if (!opts) opts = {};
+	str += buff.nonce.low.toString().padStart(11, ".");
+	str += buff.nonce.high.toString().padStart(11, ".");
+	str += buff.checksum.toString().padStart(11, ".");
+	str += buff.message.toString('base64');
 
-	if (!opts.keys) {
-		var keyRingPath = path.join(__dirname, "keyRing.js");
-		
-		if (!fs.existsSync(keyRingPath)) throw "Must supply keys or use keyRing.js";
-		
-		opts.keys = require(keyRingPath).keys;
-
-		if (!opts.keys || opts.keys.length<1) throw "Must supply keys or use keyRing.js";
-		
-	}
-
-	if (!opts.network) {
-
-		//load environment variables
-		dotenv.load();
-
-		if (!process.env.EOS_PROTOCOL || !process.env.EOS_HOST || !process.env.EOS_PORT || !process.env.EOS_CHAIN)
-			throw "Must supply network information or use .env file";
-
-		opts.network = {
-		  httpEndpoint: process.env.EOS_PROTOCOL + "://" +  process.env.EOS_HOST + ":" + process.env.EOS_PORT,
-		  chainId: process.env.EOS_CHAIN 
-		}
-
-	}
-
-	//load modules
-	var comms = require(path.join(__dirname, "src", "communications.js"));
-	var crypto = require(path.join(__dirname, "src", "crypto.js"));
-
-	//expose api with its context
-	var self = this;
-
-	self.send = comms.send.bind(opts);
-	self.scanForMessages = comms.scanForMessages.bind(opts);
-	self.encrypt = crypto.encrypt.bind(opts);
-	self.decrypt = crypto.decrypt.bind(opts);
-
-	return self;
+	return str;
 
 }
 
-module.exports = eosCommunications
+function deserialize(message){
+
+	message = message.replace("TO DECRYPT: msg.eostitan.com\n", "");
+
+	var low = parseInt(message.substring(0, 11).replace(/[.]/g, ""));
+	var high = parseInt(message.substring(11, 22).replace(/[.]/g, ""));
+	var checksum = parseInt(message.substring(22, 33).replace(/[.]/g, ""));
+	var message = message.substring(33, message.length);
+
+	var obj = {
+		nonce: new Long(low, high, 0),
+		checksum: checksum,
+		content: Buffer.from(message, "base64")
+	}
+
+	return obj;
+
+}
+
+exports.encrypt = function(fromAcct, toAcct, message){
+	let perm = acct.permissions.find(function(p){return p.perm_name=="active"});
+
+	if (!perm) new Error(`Couldn't find active permission for account ${toAcct}`);
+	if (perm.required_auth.threshold !=1) new Error(`Sending to multi-sig addresses is not currently supported.`);
+
+	let pubKey = perm.required_auth.keys[0].key;
+
+	let buff = eosecc.Aes.encrypt(key.priv_key, pubKey, message);
+
+	var str = serialize(buff);
+
+	if (str.length > 256) return new Error("error: message too long (max 256 chars)");
+}
+
+exports.decrypt = function(fromAcct, toAcct, message){
+	let perm = acct.permissions.find(function(p){return p.perm_name=="active"});
+
+	if (!perm) new Error(`Couldn't find active permission for account ${fromAcct}`);
+	if (perm.required_auth.threshold !=1) new Error(`Receiving from multi-sig addresses is not currently supported.`);
+
+	let pubKey = perm.required_auth.keys[0].key;
+
+	var deserialized = deserialize(message);
+
+	let decrypted = eosecc.Aes.decrypt(key.priv_key, pubKey, deserialized.nonce, deserialized.content, deserialized.checksum);
+
+	return decrypted.toString('utf8');
+}
