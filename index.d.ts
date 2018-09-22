d72fe94abf0c852c28e3e3fcf382b3ed024ba75b
Denis Carriere <carriere.denis@gmail.com>
2018-09-22 13:35:35 +0100

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Encrypt Message
+ *
+ * @param {string} private_key EOSIO Private Key
+ * @param {string} public_key EOSIO Public Key
+ * @param {string} message Message to Encrypt
+ * @returns {string} Encrypted Message
+ * @example
+ *
+ * const encrypted = encrypt(private_key, public_key, message);
+ */
+export function encrypt(private_key: string, public_key: string, message: string): string;
+
+/**
+ * Decrypt Message
+ *
+ * @param {string} private_key EOSIO Private Key
+ * @param {string} public_key EOSIO Public Key
+ * @param {string} message Encrypted Message
+ * @returns {string} Decrypted Message
+ * @example
+ *
+ * const decrypted = decrypt(private_key, public_key, message);
+ */
+export function decrypt(private_key: string, public_key: string, message: string): string;
